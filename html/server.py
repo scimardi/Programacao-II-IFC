78bae1a0d9e2b8a283153d2d1f9885038b3d3e53
Lima001 <limaedugabriel@gmail.com>
2019-03-21 09:09:35 -0300

    Receber da pagina web

--- html/server.py
+++ html/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pessoa import *
 app = Flask("__name__")
 
@@ -18,10 +18,16 @@ def alterar_pessoa():
 def inserir_pessoa():
     return render_template("inserir_pessoa.html")
 
-
 @app.route("/exibir_mensagem")
 def exibir_mensagem():
     return render_template("exibir_mensagem.html")
 
+@app.route("/processar_inserir")
+def add():
+    nome = request.args.get("nome")
+    endereco = request.args.get("endereco")
+    telefone = request.args.get("telefone")
+    lista.append(Pessoa(nome,endereco,telefone))
+    return listar_pessoas()
+    
 app.run(host="0.0.0.0")
-
