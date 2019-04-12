867c77100175d586fce02583d253748716f9a6a9
Lima001 <limaedugabriel@gmail.com>
2019-04-11 21:09:48 -0300

    Solução do problema = int(cpf)

--- html/server.py
+++ html/server.py
@@ -20,39 +20,39 @@ def exibir_mensagem():
 
 @app.route("/processar_inserir")
 def add():
-    cpf = request.args.get("cpf")
+    cpf = int(request.args.get("cpf"))
     nome = request.args.get("nome")
     endereco = request.args.get("endereco")
     telefone = request.args.get("telefone")
     lista.append(Pessoa(cpf,nome,endereco,telefone))
-    return listar_pessoas()
+    return redirect(url_for("iniciar"))
+   
 
 @app.route("/excluir_pessoa")
 def excluir():
 
-    chave = request.args.get("cpf")
-    #print(type(chave))
-    
-    for p in lista:
-        if p.cpf == int(chave):
-            lista.remove(p)
-    
+    chave = int(request.args.get("cpf"))
 
-    return listar_pessoas()
+    for p in lista:
+        if p.cpf == chave:
+            lista.remove(p)
+
+    return redirect(url_for("listar_pessoas"))
 
 @app.route("/form_alterar_pessoa")
 def form_alterar_pessoa():
     
-    cpf = request.args.get("cpf")
+    chave = request.args.get("cpf")
+    print(chave)
 
     for p in lista:
-        if p.cpf == cpf:
+        if p.cpf == int(chave):
             return render_template("form_alterar_pessoa.html", achei=p)
-    #return "Pessoa não encontrada"
+    return "Pessoa não encontrada"
 
 @app.route("/alterar_pessoa")
 def alterar_pessoa():
-    cpf = request.args.get("cpf")
+    cpf = int(request.args.get("cpf"))
     nome = request.args.get("nome")
     telefone = request.args.get("telefone")
     endereco = request.args.get("endereco")
@@ -63,4 +63,4 @@ def alterar_pessoa():
             return redirect(url_for("listar_pessoas"))
     return "Não achei, desculpe!"
 
-app.run(host="0.0.0.0")
+app.run()
